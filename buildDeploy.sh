bda1d8f20ddd92cdbc5d5cd690a3d31b8aabecf3
kopiprojectsx <kopiprojectsx@gmail.com>
2022-02-21 16:32:43 -0800

    updates

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -3,5 +3,6 @@ ng build --configuration production --output-path docs --base-href https://kopiP
 cp docs/index.html docs/404.html
 touch docs/.nojekyll
 echo "kopiProjectsX.github.io" >> docs/CNAME
+git add docs/*
 git commit -am "updates"
 git push

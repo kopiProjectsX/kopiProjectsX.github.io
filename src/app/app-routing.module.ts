cd0b36aad9661416d3a926ad3742eb5bb88dcbdd
kopiprojectsx <kopiprojectsx@gmail.com>
2022-02-21 16:27:50 -0800

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BeatMusicComponent } from './beat-music/beat-music.component';
 
-const routes: Routes = [];
+const routes: Routes =
+[
+  { path: 'beat',                                       component: BeatMusicComponent               },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BeatMusicComponent } from './beat-music/beat-music.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeatMusicComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule,

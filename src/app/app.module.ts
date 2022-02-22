57063854d7bf27e5210c96dccd422076e6e1a1fc
kopiprojectsx <kopiprojectsx@gmail.com>
2022-02-22 13:23:28 -0800

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { BeatMusicComponent } from './beat-music/beat-music.component';
 import { FrontpageComponent } from './frontpage/frontpage.component';
 import { MaterialModule } from 'src/helpers/modules/material.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BeatMusicComponent,
-    FrontpageComponent
+    FrontpageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
